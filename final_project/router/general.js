716a69fcb7b98f24959b2b7ec3cbd9e421614e9a
SumaSri013 <lingampallysumasri@gmail.com>
2026-02-02 00:36:31 +0530

    Update general.js

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -1,15 +1,12 @@
 const express = require('express');
 const axios = require('axios');
-let books = require("./booksdb.js");
 const public_users = express.Router();
 
 /**
- * TASK 10:
- * Get all books using async/await with Axios
+ * TASK 10: Get all books using Axios + async/await
  */
 public_users.get('/async/books', async (req, res) => {
   try {
-    // Axios call to fetch books from the same server
     const response = await axios.get('http://localhost:5000/');
     return res.status(200).json(response.data);
   } catch (error) {
@@ -18,39 +15,33 @@ public_users.get('/async/books', async (req, res) => {
 });
 
 /**
- * TASK 11:
- * Get book details by ISBN using async/await with Axios
+ * TASK 11: Get book by ISBN using Axios + async/await
  */
 public_users.get('/async/isbn/:isbn', async (req, res) => {
   try {
-    const isbn = req.params.isbn;
-
     const response = await axios.get('http://localhost:5000/');
-    const booksData = JSON.parse(response.data);
+    const books = JSON.parse(response.data);
+    const book = books[req.params.isbn];
 
-    if (booksData[isbn]) {
-      return res.status(200).json(booksData[isbn]);
-    } else {
-      return res.status(404).json({ message: "Book not found" });
+    if (book) {
+      return res.status(200).json(book);
     }
+    return res.status(404).json({ message: "Book not found" });
   } catch (error) {
     return res.status(500).json({ message: "Error fetching book by ISBN" });
   }
 });
 
 /**
- * TASK 12:
- * Get books by author using async/await with Axios
+ * TASK 12: Get books by author using Axios + async/await
  */
 public_users.get('/async/author/:author', async (req, res) => {
   try {
-    const author = req.params.author;
-
     const response = await axios.get('http://localhost:5000/');
-    const booksData = JSON.parse(response.data);
+    const books = JSON.parse(response.data);
 
-    const result = Object.values(booksData).filter(
-      book => book.author === author
+    const result = Object.values(books).filter(
+      book => book.author === req.params.author
     );
 
     if (result.length === 0) {
@@ -64,18 +55,15 @@ public_users.get('/async/author/:author', async (req, res) => {
 });
 
 /**
- * TASK 13:
- * Get books by title using async/await with Axios
+ * TASK 13: Get books by title using Axios + async/await
  */
 public_users.get('/async/title/:title', async (req, res) => {
   try {
-    const title = req.params.title;
-
     const response = await axios.get('http://localhost:5000/');
-    const booksData = JSON.parse(response.data);
+    const books = JSON.parse(response.data);
 
-    const result = Object.values(booksData).filter(
-      book => book.title === title
+    const result = Object.values(books).filter(
+      book => book.title === req.params.title
     );
 
     if (result.length === 0) {
